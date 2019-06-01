948f4234c1f738baf19349a5beb723d7c45c7940
Jorge Olmedo Santacruz Solarte <jsantacr@ccc.org.co>
2019-06-01 11:15:28 -0500

    aplicando mi primer cmmit en git

---
+++ src/test/java/co/edu/usbcali/AppTest.java
@@ -0,0 +1,26 @@
+package co.edu.usbcali;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void validacionNumerica()
+    {
+        assertTrue( !false );
+    }	
+}
